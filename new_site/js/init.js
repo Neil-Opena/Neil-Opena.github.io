fd6a8ac4e5b1c0615487065f819b663592bf8028
Neil Opena <neil1114o@gmail.com>
2019-04-27 11:52:41 -0400

    Basic journey cards format completed

--- new_site/js/init.js
+++ new_site/js/init.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+    $('.fixed-action-btn').floatingActionButton();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
